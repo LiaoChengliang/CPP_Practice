7ba467bd5cedae2b1b91e6b7f65c80038a44ac7e
liaochengliang <liaochengliang@iauto.com>
2019-07-24 14:11:11 +0800

    create pro1,pro2

---
+++ Study/pro2/main.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+//基类 Shape
+class Shape{
+public:
+	void setWidth(int w){
+		width=w;
+	}
+	void setHeight(int h){
+		height=h;
+	}
+protected:
+	int width;
+	int height;
+
+};
+//基类 PaintCost
+class PaintCost{
+public:
+	int getCost(int area){
+		return area*70;
+	}
+};
+//派生类
+class Rectangle:public Shape,public PaintCost{
+public:
+	int getArea(){
+		return (width*height);
+	}
+};
+
+
+
+
+int main(){
+	Rectangle Rect;
+	int area;
+	Rect.setHeight(7);
+	Rect.setWidth(5);
+	cout<<"Total area:"<<Rect.getArea()<<endl;
+	cout<<"Total paint cost:"<<Rect.getCost(area)<<endl;
+	return 0;
+}
